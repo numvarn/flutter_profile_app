694d3bfbe6d08a40aa5d1e7023d674b447de443e
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2024-07-10 10:29:51 +0700

    completed profile

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,76 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Image.asset("images/profile2.jpg"),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Phisan Sookkhee",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.facebook,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("facebook"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("Phone"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.share,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        Text("Share"),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Divider(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "อาจารย์ประจำสาขาวิทยากาคอมพิวเตอร์",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
